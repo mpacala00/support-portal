c8cc555700cc2d237d419f8c07b9660ba6ef8c5f
mpacala00 <mpacala00@gmail.com>
2020-09-25 14:36:33 +0200

    Implementing custom 403 status response and AccessDeniedHandler

---
+++ src/main/java/com/github/mpacala00/supportportal/filter/JwtAccessDeniedHandler.java
@@ -0,0 +1,39 @@
+package com.github.mpacala00.supportportal.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mpacala00.supportportal.constant.SecurityConstant;
+import com.github.mpacala00.supportportal.domain.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                       AccessDeniedException exception) throws IOException, ServletException {
+
+        HttpResponse httpResponse =
+                new HttpResponse(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED,
+                        HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase(),
+                        SecurityConstant.ACCESS_DENIED_MESSAGE );
+
+        httpServletResponse.setContentType(APPLICATION_JSON_VALUE); //import from MediaType
+        httpServletResponse.setStatus(HttpStatus.FORBIDDEN.value());
+
+        OutputStream outputStream = httpServletResponse.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
---
+++ src/main/java/com/github/mpacala00/supportportal/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,43 @@
+package com.github.mpacala00.supportportal.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.mpacala00.supportportal.constant.SecurityConstant;
+import com.github.mpacala00.supportportal.domain.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/**
+ * This class replaces the default implementation of Spring Security 403 Forbidden
+ * It gets triggered when user does not have the required authentication
+ */
+
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException {
+
+        HttpResponse httpResponse =
+                new HttpResponse(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN,
+                HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(),
+                SecurityConstant.FORBIDDEN_MESSAGE);
+
+        response.setContentType(APPLICATION_JSON_VALUE); //import from MediaType
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+
+        //response.sendError(403, "Access Denied");
+    }
+}
