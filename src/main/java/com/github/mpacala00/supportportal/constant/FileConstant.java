20f9b5da4d605be9aba751934b285071d7d450e6
mpacala00 <mpacala00@gmail.com>
2021-01-22 15:56:48 +0100

    Adding new methods to UserService and FileConstant

---
+++ src/main/java/com/github/mpacala00/supportportal/constant/FileConstant.java
@@ -0,0 +1,13 @@
+package com.github.mpacala00.supportportal.constant;
+
+public class FileConstant {
+    public static final String USER_IMAGE_PATH = "/user/image/";
+    public static final String JPG_EXTENSION = "jpg";
+    public static final String USER_FOLDER = System.getProperty("user.home") + "/supportportal/user/";
+    public static final String DIR_CREATED = "Created directory for: ";
+    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile";
+    public static final String FILE_SAVED = "Saved file in the system by name: ";
+    public static final String DOT = ".";
+    public static final String FORWARD_SLASH = "/";
+    public static final String TEMP_PROFILE_IMAGE_BASE_URL = "https://robohash.org/";
+}
--- src/main/java/com/github/mpacala00/supportportal/service/UserService.java
+++ src/main/java/com/github/mpacala00/supportportal/service/UserService.java
@@ -4,6 +4,7 @@ import com.github.mpacala00.supportportal.domain.User;
 import com.github.mpacala00.supportportal.exception.domain.EmailExistsException;
 import com.github.mpacala00.supportportal.exception.domain.UserNotFoundException;
 import com.github.mpacala00.supportportal.exception.domain.UsernameExistsException;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,4 +16,17 @@ public interface UserService {
     User register(String firstName, String lastName, String username, String email) throws UsernameExistsException, EmailExistsException, UserNotFoundException;
 
     List<User> getUsers();
+
+    //for adding user as a super admin, for example
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNotLocked,
+                    boolean isActive, MultipartFile profileImage);
+
+    //we need to know existing username to update its account, could also be done with user's id or just with
+    //one method in interface and 2 implementations of it in the actual service
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail,
+                    String role, boolean isNotLocked, boolean isActive, MultipartFile profileImage);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile newProfileImage);
 }
