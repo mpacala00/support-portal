71a058785cdcd0945f81171ad0c69d1d07cc4678
mpacala00 <mpacala00@gmail.com>
2021-01-27 16:56:29 +0100

    Implementing front-end auth service

--- support-portal-ng/src/app/app.module.ts
+++ support-portal-ng/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule
+   ],
+   providers: [CookieService],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ support-portal-ng/src/app/service/authentication.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { CookieService } from 'ngx-cookie-service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class AuthenticationService {
+   private host = environment.baseAPIUrl;
+   private token: string;
+   private loggedUser: string;
+   private jwtHelper = new JwtHelperService();
+
+   constructor(private http: HttpClient, private cookies: CookieService) { }
+
+   public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+
+      //post function args: (url, object (body), meta-data (like headers))
+      //observe: 'response' will return the entire response with headers etc., without it we would only get body
+      return this.http
+         .post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, { observe: 'response' });
+   }
+
+   public register(user: User): Observable<User | HttpErrorResponse> {
+      return this.http
+         .post<User | HttpErrorResponse>(`${this.host}/user/register`, user, { observe: 'response' });
+   }
+
+   //using cookies for storage of JWT is safer than localStorage
+   public logout(): void {
+      this.token = null;
+      this.loggedUser = null;
+      this.cookies.deleteAll();
+   }
+
+   public isLoggedIn(): boolean {
+      this.loadToken();
+
+      //check if not empty
+      if (this.token != null && this.token != '') {
+         let sub = this.jwtHelper.decodeToken(this.token).sub;
+         //check if sub not empty
+         if (sub != null && sub != '') {
+            //check if not expired
+            if (!this.jwtHelper.isTokenExpired(this.token)) {
+               //subject of the token is username, as was specified in the api
+               this.loggedUser = this.jwtHelper.decodeToken(this.token).sub;
+               return true;
+            }
+         }
+      } else {
+         this.logout();
+         return false;
+      }
+   }
+
+   public saveToken(token: string): void {
+      this.token = token;
+
+      //this.cookies.set(name, value, {expires?, path?, domain?, secure?, sameSite?})
+      this.cookies.set('token', token);
+   }
+
+   public getToken(): string {
+      return this.token;
+   }
+
+   public loadToken(): void {
+      this.token = this.cookies.get('token');
+   }
+
+   public addUserToLocalCache(user: User): void {
+      this.cookies.set('user', JSON.stringify(user));
+   }
+
+   public getUserFromLocalCache(): User {
+      return JSON.parse(this.cookies.get('user'));
+   }
+
+}
