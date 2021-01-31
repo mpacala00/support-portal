11f40bf9adf1835db21ab0ac5061cbb1a978169d
mpacala00 <mpacala00@gmail.com>
2021-01-31 17:17:13 +0100

    Adding front-end components and setting up routes

--- support-portal-ng/src/app/app-routing.module.ts
+++ support-portal-ng/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { UserComponent } from './component/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'login', component: LoginComponent },
+   { path: 'register', component: RegisterComponent },
+   { path: 'user/management', component: UserComponent },
+   //order is important, this has to be last one
+   { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- support-portal-ng/src/app/app.module.ts
+++ support-portal-ng/src/app/app.module.ts
@@ -12,10 +12,16 @@ import { AuthenticationGuard } from './guard/authentication.guard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { UserComponent } from './component/user/user.component';
 
 @NgModule({
    declarations: [
-      AppComponent
+      AppComponent,
+      LoginComponent,
+      RegisterComponent,
+      UserComponent
    ],
    imports: [
       BrowserModule,
